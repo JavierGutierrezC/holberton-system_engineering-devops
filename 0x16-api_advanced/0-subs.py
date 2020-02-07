7aec490753210b3939fa36f94cf0fe688472fbe0
Javier.Gutierrez.C <javier.gutierrez.cruz@gmail.com>
2020-02-07 01:20:14 +0000

    else

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Returns number of subscribers
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    subreds = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    red_req = requests.get(subreds, headers={'User-Agent': 'lee'})
+    if (red_req.status_code == requests.codes.ok):
+        jason_full = red_req.json()
+        return(jason_full['data']['subscribers'])
+    else:
+        return (0)
